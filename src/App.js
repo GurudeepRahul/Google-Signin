ef3d9e4e21ab0d0f20a01eb644b179fe39f5e373
gururah1 <gkandula@mathongo.com>
2024-07-24 20:08:37 +0530

    Code for Google Auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import {useEffect} from 'react';
 import './App.css';
 
 function App() {
+  function handleCallBackResponse (response) {
+    console.log(response.credential);
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id : "904101667242-5h26poo8dvdns15pp2a504nv3u2fodme.apps.googleusercontent.com",
+      callback: handleCallBackResponse
+    })
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {theme: "outline", size: "large"}
+    )
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="signInDiv"></div>
     </div>
   );
 }
